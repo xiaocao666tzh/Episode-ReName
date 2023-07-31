07e9eb5c6784323a7f212d062efda9ffb5b02f8b
xcao <52877059+xiaocao666tzh@users.noreply.github.com>
2023-07-31 15:52:19 +0800

    fix Bug 修复VCB的识别问题

--- EpisodeReName.py
+++ EpisodeReName.py
@@ -749,7 +749,7 @@ if os.path.isdir(target_path):
                 if group == '' and file_path.find('7³ACG') != -1:
                     group = '7³ACG'
             stream = get_stream_in_name(name)
-            if file_name.find('VCB-Studio') == True:
+            if file_name.find('VCB-Studio') != -1:
                 stream = 'BDRip'
             encode = get_encode_in_name(name)
             logger.info(f'{season, ep}')
@@ -797,7 +797,7 @@ else:
                 group = '7³ACG'
         stream = get_stream_in_name(file_name)
         encode = get_encode_in_name(file_name)
-        if file_name.find('VCB-Studio') == True:
+        if file_name.find('VCB-Studio') != -1:
             stream = 'BDRip'
         if season and ep:
             # 修正集数
